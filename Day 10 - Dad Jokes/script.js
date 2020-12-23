2741019fd9c550045140f4b7a8bd9453093fa9f0
Ibrahim Elsakka <ibrahim.elsakka@gmail.com>
2020-12-23 10:03:48 -0800

    Completed day 10 project

---
+++ Day 10 - Dad Jokes/script.js
@@ -0,0 +1,35 @@
+const jokeElement = document.getElementById('joke');
+const jokeBtn = document.getElementById('jokeBtn');
+
+jokeBtn.addEventListener('click', generateJoke)
+
+generateJoke();
+
+// Using async await
+async function generateJoke() {
+	const config = {
+		headers: {
+			"Accept": "application/json"
+		}
+	};
+	const response = await fetch('https://icanhazdadjoke.com', config);
+
+	const data = await response.json();
+
+	jokeElement.innerHTML = data.joke;
+}
+
+// Using fetch.then()
+// function generateJoke() {
+// 	const config = {
+// 		headers: {
+// 			"Accept": "application/json"
+// 		}
+// 	}
+// 	fetch('https://icanhazdadjoke.com', config)
+// 	.then(response => response.json())
+// 	.then(data => {
+// 		jokeElement.innerHTML = data.joke
+// 	})
+
+// }
